66e4d294ecfcd30f760026c5708376545bdd6496
linaaqu <latihewa2006@gmail.com>
2025-03-19 10:28:57 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,18 +1,44 @@
-def factorial(n):
-    if n < 0:
-        return "Факториал не определен для отрицательных чисел"
-    # elif n == 0 or n == 1:
-    #     return 1
-    # else:
-    #     result = 1
-    #     for i in range(2, n + 1):
-    #         result *= i
-    #     return result
-    if n < 2:
+from math import factorial
+
+def fact(x):
+    if(x == 0):
         return 1
-    else:
-        return n * factorial(n - 1)
+    return factorial(x)
+
+print(fact(0))
 
 
-print(factorial(5))
 
+def simple(q):
+    if q <= 1:
+        return "Составное"
+    for i in range(2, q):
+        if q % i == 0:
+            return "Составное"
+    return "Простое"
+
+
+
+print(simple(5))
+
+def elements(input_list):
+    return list(set(input_list))
+
+my_list = [1, 2, 2, 3, 4, 4, 5]
+unique_list = elements(my_list)
+print(unique_list)
+
+
+
+def group_words(words):
+    list = {}
+    for word in words:
+        length = len(word)
+        if length not in list:
+            list[length] = []
+        list[length].append(word)
+    return list
+
+words_list = ["apple", "banana", "pear", "kiwi", "grape", "fig"]
+result = group_words(words_list)
+print(result)
