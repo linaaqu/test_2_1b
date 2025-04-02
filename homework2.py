97a13f00fea51e6f2b64b290517a65ad9717dba6
linaaqu <latihewa2006@gmail.com>
2025-04-02 10:08:06 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -10,16 +10,14 @@ print(fact(0))
 
 
 def simple(q):
+    if q == 1:
+        return "Простое"
     if q <= 1:
         return "Ни простое, ни составное"
     for i in range(2, q):
         if q % i == 0:
             return "Составное"
     return "Простое"
-
-
-def elements(input_list):
-    return list(set(input_list))
 
 my_list = [1, 2, 2, 3, 4, 4, 5]
 unique_list = elements(my_list)
